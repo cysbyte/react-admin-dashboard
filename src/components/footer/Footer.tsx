9fb0efaccaaed2bca3f92394f78924113131387f
chenyunshu2 <chenyunshu2@gmail.com>
2023-12-26 16:49:35 +0800

    create grid layout for home page

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+const Footer = () => {
+  return (
+      <div className='footer'>
+          <span>Admin dashboard</span>
+          <span>Developed by cysbyte</span>
+    </div>
+  )
+}
+
+export default Footer
