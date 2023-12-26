f3d6dc096853d6b95938dc5e4db46cd540296746
chenyunshu2 <chenyunshu2@gmail.com>
2023-12-27 00:12:13 +0800

    add responsive css

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,9 +8,9 @@ const Navbar = () => {
               <span>Admin Dashboard</span>
           </div>
           <div className='icons'>
-              <img src="/search.svg" alt="icon" />
-              <img src="/app.svg" alt="icon" />
-              <img src="/expand.svg" alt="icon" />
+              <img src="/search.svg" alt="icon" className="icon"/>
+              <img src="/app.svg" alt="icon" className="icon"/>
+              <img src="/expand.svg" alt="icon" className="icon"/>
               <div className='notification'>
                   <img src="/notifications.svg" alt="" />
                   <span>1</span>
@@ -19,7 +19,7 @@ const Navbar = () => {
                   <img src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
                   <span>Jane</span>
               </div>
-              <img src="/settings.svg" alt="icon" />
+              <img src="/settings.svg" alt="icon" className="icon"/>
           </div>
     </div>
   )
